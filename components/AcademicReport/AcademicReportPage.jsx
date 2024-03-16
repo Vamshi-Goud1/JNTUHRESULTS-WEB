745014c6a87d164e93f57e61ad87f9e265256267
Vamshi Goud <124991557+Vamshi-Goud1@users.noreply.github.com>
2024-03-16 13:48:38 +0530

    Update AcademicReportPage.jsx

--- components/AcademicReport/AcademicReportPage.jsx
+++ components/AcademicReport/AcademicReportPage.jsx
@@ -39,7 +39,7 @@ const AcademicReportPage = ({ backlog }) => {
 
        
         //const url = "/api/academicresult?htno=" + htno;
-        const url = "http://127.0.0.1:8000/api/academicresult?htno=" + htno;
+        const url = "https://jntuhresults.vercel.app/api/academicresult?htno=" + htno;
         try {
             //const response = await axios.get(url);
             const response = await axios.get(url, { mode: 'cors', timeout: 5000, });
